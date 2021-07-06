0645901671535ab0800bb6b1fccc10f546723008
Valera Tomashevskyi <darth2000vader@gmail.com>
2021-07-06 13:50:40 +0300

    Added some tabs in reviews block

---
+++ script.js
@@ -0,0 +1,22 @@
+const allButtons = document.querySelectorAll(".tab_button");
+const allTabs = document.querySelectorAll(".reviews_item");
+
+allButtons.forEach(function(item){
+    item.addEventListener("click", function(){
+        let currentButton = item;
+        let tabId = currentButton.getAttribute("data-tab");
+
+        let currentTab = document.querySelector(tabId);
+
+        allButtons.forEach(function(item){
+            item.classList.remove('active');
+        });
+
+        allTabs.forEach(function(item){
+            item.classList.remove('active');
+        });
+
+        currentButton.classList.add('active');
+        currentTab.classList.add('active');
+    });
+});
